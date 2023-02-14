d5c1ed4abf3d01837ff42f3a2c23a7a7266a9e4c
sistemasfinorleonel <96667421+sistemasfinorleonel@users.noreply.github.com>
2023-02-14 18:59:35 -0400

    agregado

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const cors =require('cors');
+const path =require('path');
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 //Crear el servidor /aplicacion de express
@@ -20,6 +21,16 @@ app.use(cors());
 app.use(express.json());
 //RUTAS
 app.use('/api/auth',require('./routes/auth'))
+//
+//MANEJAR LAS DEMAS RUTAS
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'public/index.html'));
+});
+
+
+
+
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`servidor corriendo en puerto ${process.env.PORT}`)
